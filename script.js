1658bbb0a6fdc9c8497e98a56cfa3abe018080b8
Maxwell Stevenson <152032246+MaxwellStevenson@users.noreply.github.com>
2024-01-18 18:53:31 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let command = '';
+    const outputArea = document.getElementById('Command_Line');
 
-document.addEventListener('keydown', function (event) {
-  if (event.keyCode === 13) {
-   
-    command = document.getElementById('userInput').value;
+    document.addEventListener('keydown', function (event) {
+      if (event.keyCode === 13) {
+        command = document.getElementById('userInput').value;
 
-    if (command === 'date') {
-      const d = new Date();
-      document.getElementById("Command_Line").innerHTML = d;
-    }
+        if (command === 'date') {
+          const d = new Date();
+          outputArea.innerHTML += `<p>${d}</p>`;
+        } else if (command === 'help') {
+          outputArea.innerHTML += "<p>The only command is 'date' for now.</p>";
+        } else {
+          outputArea.innerHTML += `<p>Command '${command}' is not recognized... for now.</p>`;
+        }
 
-    if (command === 'help') {
-        document.getElementById("Command_Line").innerHTML = "the only one is date, for now";
+        // Clear the input field
+        document.getElementById('userInput').value = '';
       }
-
-  }
-});
+    });
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   
