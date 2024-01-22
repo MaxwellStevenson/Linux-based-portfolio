bbc65649f440606f65372566e172ae8b8d04c88f
Maxwell Stevenson <152032246+MaxwellStevenson@users.noreply.github.com>
2024-01-22 15:49:00 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Portfolio
 A description of me and what I like to do. 
-
-Also a template do if anyone wants to copy me they can, with a simple button.
--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 let command = '';
-    const outputArea = document.getElementById('Command_Line');
+const outputArea = document.getElementById('Command_Line');
 
-    document.addEventListener('keydown', function (event) {
-      if (event.keyCode === 13) {
-        command = document.getElementById('userInput').value;
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    command = document.getElementById('userInput').value;
 
-        if (command === 'date') {
-          const d = new Date();
-          outputArea.innerHTML += `<p>${d}</p>`;
-        } else if (command === 'help') {
-          outputArea.innerHTML += "<p>The only command is 'date' for now.</p>";
-        } else {
-          outputArea.innerHTML += `<p>Command '${command}' is not recognized... for now.</p>`;
-        }
-
-        // Clear the input field
-        document.getElementById('userInput').value = '';
-      }
-    });
+    if (command.startsWith('echo ')) {
+      const echoedText = command.substring(5); // Extract text after 'echo '
+      outputArea.innerHTML += `<p>${echoedText}</p>`;
+    } else if (command === 'date') {
+      const d = new Date();
+      outputArea.innerHTML += `<p>${d}</p>`;
+    } else if (command === 'help') {
+      outputArea.innerHTML += "<p>Commands:</p><br><p>echo- repeats phrases</p><span class='line-break'></span><p>whoami- Who am I? (Pronounced woah-mi)</p><span class='line-break'></span><p>clear- clears all previous commands</p><span class='line-break'></span><p>date- displays the time and date</p>";
+    } else if (command === 'clear') {
+      outputArea.innerHTML = ''; // Clear the output area
+    } else if (command === 'echo') {
+      outputArea.innerHTML = '<p>Make sure to have a phrase after this command!</p>';
+    } else if (command === 'whoami') {
+      outputArea.innerHTML = '<p>My name is Maxwell Stevenson, I am a highschool freshman that attends Alta Highschool, I like everything software, hardware and rap music</p>';
+    } else {
+      outputArea.innerHTML += `<p>Command '${command}' is not recognized... for now.</p>`;
+    }
+    // Clear the input field
+    document.getElementById('userInput').value = '';
+  }
+});
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  
+  //console.log(navigator.userAgent)
   window.location.href = "mobile.html";
 } else {
-   //nothing, they are on pc
+  // Nothing, they are on PC
 }
+
+
+document.getElementById('userInput').focus();
+
+userInput.addEventListener('blur', function () {
+  userInput.focus();
+});
